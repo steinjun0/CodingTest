38118555a57525fbbc5dc320ad93423d4ddd7289
steinjun0 <1142308@naver.com>
2023-01-20 09:07:37 +0900

    template 주석 제거

--- template.js
+++ template.js
@@ -1,16 +1,15 @@
 function solve() {
-    let answer;
-    return answer;
-  }
-  
-  const filePath =
-    process.platform === "linux" ? "/dev/stdin" : "/Users/seogjun-yeong/Desktop/Programming/CodingTest/testcase.txt";
-  const input = require("fs")
-    .readFileSync(filePath)
-    .toString()
-    .trim()
-    .split("\n")
-    .map((row) => row.split(" ").map((num) => +num));
-    // +: 단항 연산자입니다. 피연산자가 숫자 타입이 아니면 숫자로 변환을 시도합니다. ex) +"3" -> 3
+  let answer;
+  return answer;
+}
 
-  console.log(solve());
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "/Users/seogjun-yeong/Desktop/Programming/CodingTest/testcase.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((row) => row.split(" ").map((num) => +num));
+
+console.log(solve());

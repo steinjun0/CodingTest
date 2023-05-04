39b7b365b860d41596eb6414935cbc6da424dfdb
steinjun0 <1142308@naver.com>
2023-05-04 10:09:44 +0900

    enhance performance 18430

--- Backtracking/gold/18430-무기 공학.js
+++ Backtracking/gold/18430-무기 공학.js
@@ -19,36 +19,53 @@ function getScore(bmr,x,y){
     }
 }
 function solve(N,M,woods){
-    const isValid = (x,y)=>0<=x&&x<N&&0<=y&&y<M
+    function isValid(x,y){return 0<=x&&x<N&&0<=y&&y<M}
     const isVisited = Array.from(Array(N),()=>Array(M).fill(false))
 
     let max=0
     function dfs(pos,value){
         // console.table(isVisited)
-        max = Math.max(max,value)
+        if(max < value){
+            max = value
+        }
         if(pos >= N*M) return
         const x = ~~(pos/M)
         const y = pos%M
         outer: for(const bmr of bmrs){
-            // bmr이 들어갈 수 있는가?
-            for(const [dx,dy] of bmr){
-                const nx = x+dx
-                const ny = y+dy
-                if(!isValid(nx,ny) || isVisited[nx][ny]) continue outer
+            if(bmr.length === 0){
+                dfs(pos+1,value+getScore(bmr,x,y))
             }
-            // console.table(bmr)
-            // 가능하다면
-            for(const [dx,dy] of bmr){
-                const nx = x+dx
-                const ny = y+dy
-                isVisited[nx][ny] = true
+            else if(
+                isValid(bmr[0][0]+x,bmr[0][1]+y) &&
+                isValid(bmr[1][0]+x,bmr[1][1]+y) &&
+                isValid(bmr[2][0]+x,bmr[2][1]+y) &&
+                !isVisited[bmr[0][0]+x][bmr[0][1]+y] &&
+                !isVisited[bmr[1][0]+x][bmr[1][1]+y] &&
+                !isVisited[bmr[2][0]+x][bmr[2][1]+y]
+            ){
+                isVisited[bmr[0][0]+x][bmr[0][1]+y] =isVisited[bmr[1][0]+x][bmr[1][1]+y] = isVisited[bmr[2][0]+x][bmr[2][1]+y]= true
+                dfs(pos+1,value+getScore(bmr,x,y))
+                isVisited[bmr[0][0]+x][bmr[0][1]+y] =isVisited[bmr[1][0]+x][bmr[1][1]+y] = isVisited[bmr[2][0]+x][bmr[2][1]+y]= false
             }
-            dfs(pos+1,value+getScore(bmr,x,y))
-            for(const [dx,dy] of bmr){
-                const nx = x+dx
-                const ny = y+dy
-                isVisited[nx][ny] = false
-            }
+            // // bmr이 들어갈 수 있는가?
+            // for(const [dx,dy] of bmr){
+            //     const nx = x+dx
+            //     const ny = y+dy
+            //     if(!isValid(nx,ny) || isVisited[nx][ny]) continue outer
+            // }
+            // // console.table(bmr)
+            // // 가능하다면
+            // for(const [dx,dy] of bmr){
+            //     const nx = x+dx
+            //     const ny = y+dy
+            //     isVisited[nx][ny] = true
+            // }
+            // dfs(pos+1,value+getScore(bmr,x,y))
+            // for(const [dx,dy] of bmr){
+            //     const nx = x+dx
+            //     const ny = y+dy
+            //     isVisited[nx][ny] = false
+            // }
         }
     }    
     dfs(0,0)
@@ -59,3 +76,5 @@ console.log(solve(N,M,woods))
 
 // 00:41:48 틀렸습니다(2%) -> 마지막 블럭 예외처리 추가
 // 00:44:49 맞았습니다! -> 최적화 모르겠음...
+// 00:59:08 for문 제거 -> 264ms
+// 01:00:40 함수 표현식 => 함수 선언문으로 변경 -> 264ms

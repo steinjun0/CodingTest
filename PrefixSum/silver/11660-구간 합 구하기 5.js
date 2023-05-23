3f0727b51ba05f5cfd7ca4f75c61866a09f96275
steinjun0 <1142308@naver.com>
2023-05-23 10:04:39 +0900

    11660 refactoring

--- PrefixSum/silver/11660-구간 합 구하기 5.js
+++ PrefixSum/silver/11660-구간 합 구하기 5.js
@@ -7,14 +7,13 @@ const [[N,M],...numbers] = require('fs').readFileSync(process.platform === 'linu
 const prefix = Array.from(Array(N),()=>Array(N).fill(0))
 
 for(let i=0;i<N;i++){
-    for(let j=0;j<N;j++){
-        prefix[i][j] = (prefix[i][j-1] ?? 0) + (numbers[i][j])
-    }
+    prefix[i][0] = numbers[i][0]
+    prefix[0][i] = numbers[0][i]
 }
 
 for(let i=1;i<N;i++){
-    for(let j=0;j<N;j++){
-        prefix[i][j] += (prefix[i-1] ? prefix[i-1][j] : 0)
+    for(let j=1;j<N;j++){
+        prefix[i][j] = prefix[i][j-1] + prefix[i-1][j] -prefix[i-1][j-1] + numbers[i][j]
     }
 }
 
